3055ccebe834f66a55f29ca801eb72f9ea5323d9
gphoenix14 <104388188+gphoenix14@users.noreply.github.com>
2024-05-25 14:55:02 +0200

    ok dapper

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using EmployeeManagement.Models;
-
 
 namespace EmployeeManagement
 {
@@ -8,8 +9,9 @@ namespace EmployeeManagement
     {
         public static void Main(string[] args)
         {
+            EmployeeDataAccessLayerWithDAO employeeDAL_ADO = new EmployeeDataAccessLayerWithDAO();
+            EmployeeDataAccessLayerWithDapper employeeDAL_Dapper = new EmployeeDataAccessLayerWithDapper();
 
-            EmployeeDataAccessLayerWithDAO employeeDAL = new EmployeeDataAccessLayerWithDAO();
             while (true)
             {
                 Console.WriteLine("Seleziona un'opzione:");
@@ -25,19 +27,19 @@ namespace EmployeeManagement
                 switch (scelta)
                 {
                     case 1:
-                        CreaUtente(employeeDAL);
+                        CreaUtente(employeeDAL_ADO, employeeDAL_Dapper);
                         break;
                     case 2:
-                        VisualizzaUtente(employeeDAL);
+                        VisualizzaUtente(employeeDAL_ADO, employeeDAL_Dapper);
                         break;
                     case 3:
-                        AggiornaUtente(employeeDAL);
+                        AggiornaUtente(employeeDAL_ADO, employeeDAL_Dapper);
                         break;
                     case 4:
-                        ListaTuttiGliUtenti(employeeDAL);
+                        ListaTuttiGliUtenti(employeeDAL_ADO, employeeDAL_Dapper);
                         break;
                     case 5:
-                        EliminaUtente(employeeDAL);
+                        EliminaUtente(employeeDAL_ADO, employeeDAL_Dapper);
                         break;
                     case 6:
                         return;
@@ -48,7 +50,7 @@ namespace EmployeeManagement
             }
         }
 
-        static void CreaUtente(EmployeeDataAccessLayerWithDAO employeeDAL)
+        static void CreaUtente(EmployeeDataAccessLayerWithDAO employeeDAL_ADO, EmployeeDataAccessLayerWithDapper employeeDAL_Dapper)
         {
             Employee employee = new Employee();
 
@@ -61,17 +63,141 @@ namespace EmployeeManagement
             Console.Write("Inserisci la città: ");
             employee.City = Console.ReadLine();
 
-            employeeDAL.AddEmployee(employee);
-            Console.WriteLine("Utente creato con successo.");
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            employeeDAL_ADO.AddEmployee(employee);
+            stopwatch.Stop();
+            Console.WriteLine("Utente creato con successo usando ADO.NET in " + stopwatch.ElapsedMilliseconds + " ms.");
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            employeeDAL_Dapper.AddEmployee(employee);
+            stopwatch.Stop();
+            Console.WriteLine("Utente creato con successo usando Dapper in " + stopwatch.ElapsedMilliseconds + " ms.");
         }
 
-        static void VisualizzaUtente(EmployeeDataAccessLayerWithDAO employeeDAL)
+        static void VisualizzaUtente(EmployeeDataAccessLayerWithDAO employeeDAL_ADO, EmployeeDataAccessLayerWithDapper employeeDAL_Dapper)
         {
             Console.Write("Inserisci l'ID dell'utente: ");
             int id = Convert.ToInt32(Console.ReadLine());
 
-            Employee employee = employeeDAL.GetEmployeeData(id);
+            Stopwatch stopwatch = new Stopwatch();
 
+            stopwatch.Start();
+            Employee employee_ADO = employeeDAL_ADO.GetEmployeeData(id);
+            stopwatch.Stop();
+            Console.WriteLine("Dati utente usando ADO.NET ottenuti in " + stopwatch.ElapsedMilliseconds + " ms.");
+            StampaDatiUtente(employee_ADO);
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            Employee employee_Dapper = employeeDAL_Dapper.GetEmployeeData(id);
+            stopwatch.Stop();
+            Console.WriteLine("Dati utente usando Dapper ottenuti in " + stopwatch.ElapsedMilliseconds + " ms.");
+            StampaDatiUtente(employee_Dapper);
+        }
+
+        static void AggiornaUtente(EmployeeDataAccessLayerWithDAO employeeDAL_ADO, EmployeeDataAccessLayerWithDapper employeeDAL_Dapper)
+        {
+            Console.Write("Inserisci l'ID dell'utente: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            Employee employee_ADO = employeeDAL_ADO.GetEmployeeData(id);
+            Employee employee_Dapper = employeeDAL_Dapper.GetEmployeeData(id);
+
+            if (employee_ADO != null && employee_Dapper != null)
+            {
+                Console.Write("Inserisci il nuovo nome (lascia vuoto per mantenere l'attuale): ");
+                string name = Console.ReadLine();
+                if (!string.IsNullOrEmpty(name))
+                {
+                    employee_ADO.Name = name;
+                    employee_Dapper.Name = name;
+                }
+
+                Console.Write("Inserisci il nuovo genere (lascia vuoto per mantenere l'attuale): ");
+                string gender = Console.ReadLine();
+                if (!string.IsNullOrEmpty(gender))
+                {
+                    employee_ADO.Gender = gender;
+                    employee_Dapper.Gender = gender;
+                }
+
+                Console.Write("Inserisci il nuovo dipartimento (lascia vuoto per mantenere l'attuale): ");
+                string department = Console.ReadLine();
+                if (!string.IsNullOrEmpty(department))
+                {
+                    employee_ADO.Department = department;
+                    employee_Dapper.Department = department;
+                }
+
+                Console.Write("Inserisci la nuova città (lascia vuoto per mantenere l'attuale): ");
+                string city = Console.ReadLine();
+                if (!string.IsNullOrEmpty(city))
+                {
+                    employee_ADO.City = city;
+                    employee_Dapper.City = city;
+                }
+
+                Stopwatch stopwatch = new Stopwatch();
+
+                stopwatch.Start();
+                employeeDAL_ADO.UpdateEmployee(employee_ADO);
+                stopwatch.Stop();
+                Console.WriteLine("Utente aggiornato con successo usando ADO.NET in " + stopwatch.ElapsedMilliseconds + " ms.");
+
+                stopwatch.Reset();
+                stopwatch.Start();
+                employeeDAL_Dapper.UpdateEmployee(employee_Dapper);
+                stopwatch.Stop();
+                Console.WriteLine("Utente aggiornato con successo usando Dapper in " + stopwatch.ElapsedMilliseconds + " ms.");
+            }
+            else
+            {
+                Console.WriteLine("Utente non trovato.");
+            }
+        }
+
+        static void ListaTuttiGliUtenti(EmployeeDataAccessLayerWithDAO employeeDAL_ADO, EmployeeDataAccessLayerWithDapper employeeDAL_Dapper)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            var employees_ADO = employeeDAL_ADO.GetAllEmployees();
+            stopwatch.Stop();
+            Console.WriteLine("Lista utenti usando ADO.NET ottenuta in " + stopwatch.ElapsedMilliseconds + " ms.");
+            StampaListaUtenti(employees_ADO);
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            var employees_Dapper = employeeDAL_Dapper.GetAllEmployees();
+            stopwatch.Stop();
+            Console.WriteLine("Lista utenti usando Dapper ottenuta in " + stopwatch.ElapsedMilliseconds + " ms.");
+            StampaListaUtenti(employees_Dapper);
+        }
+
+        static void EliminaUtente(EmployeeDataAccessLayerWithDAO employeeDAL_ADO, EmployeeDataAccessLayerWithDapper employeeDAL_Dapper)
+        {
+            Console.Write("Inserisci l'ID dell'utente da eliminare: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            employeeDAL_ADO.DeleteEmployee(id);
+            stopwatch.Stop();
+            Console.WriteLine("Utente eliminato con successo usando ADO.NET in " + stopwatch.ElapsedMilliseconds + " ms.");
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            employeeDAL_Dapper.DeleteEmployee(id);
+            stopwatch.Stop();
+            Console.WriteLine("Utente eliminato con successo usando Dapper in " + stopwatch.ElapsedMilliseconds + " ms.");
+        }
+
+        static void StampaDatiUtente(Employee employee)
+        {
             if (employee != null)
             {
                 Console.WriteLine("ID: " + employee.ID);
@@ -86,43 +212,8 @@ namespace EmployeeManagement
             }
         }
 
-        static void AggiornaUtente(EmployeeDataAccessLayerWithDAO employeeDAL)
+        static void StampaListaUtenti(IEnumerable<Employee> employees)
         {
-            Console.Write("Inserisci l'ID dell'utente: ");
-            int id = Convert.ToInt32(Console.ReadLine());
-
-            Employee employee = employeeDAL.GetEmployeeData(id);
-
-            if (employee != null)
-            {
-                Console.Write("Inserisci il nuovo nome (lascia vuoto per mantenere l'attuale): ");
-                string name = Console.ReadLine();
-                if (!string.IsNullOrEmpty(name)) employee.Name = name;
-
-                Console.Write("Inserisci il nuovo genere (lascia vuoto per mantenere l'attuale): ");
-                string gender = Console.ReadLine();
-                if (!string.IsNullOrEmpty(gender)) employee.Gender = gender;
-
-                Console.Write("Inserisci il nuovo dipartimento (lascia vuoto per mantenere l'attuale): ");
-                string department = Console.ReadLine();
-                if (!string.IsNullOrEmpty(department)) employee.Department = department;
-
-                Console.Write("Inserisci la nuova città (lascia vuoto per mantenere l'attuale): ");
-                string city = Console.ReadLine();
-                if (!string.IsNullOrEmpty(city)) employee.City = city;
-
-                employeeDAL.UpdateEmployee(employee);
-                Console.WriteLine("Utente aggiornato con successo.");
-            }
-            else
-            {
-                Console.WriteLine("Utente non trovato.");
-            }
-        }
-
-        static void ListaTuttiGliUtenti(EmployeeDataAccessLayerWithDAO employeeDAL)
-        {
-            var employees = employeeDAL.GetAllEmployees();
             foreach (var employee in employees)
             {
                 Console.WriteLine("ID: " + employee.ID);
@@ -133,15 +224,5 @@ namespace EmployeeManagement
                 Console.WriteLine();
             }
         }
-
-        static void EliminaUtente(EmployeeDataAccessLayerWithDAO employeeDAL)
-        {
-            Console.Write("Inserisci l'ID dell'utente da eliminare: ");
-            int id = Convert.ToInt32(Console.ReadLine());
-
-            employeeDAL.DeleteEmployee(id);
-            Console.WriteLine("Utente eliminato con successo.");
-        }
     }
-    
 }
---
+++ EmployeeManagement/service/EmployeeDataAccessLayerWithDapper.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace EmployeeManagement.Models
+{
+    public class EmployeeDataAccessLayerWithDapper
+    {
+        string connectionString = "Server=localhost;Database=employee;User Id=employee;Password=employee;";
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            using (IDbConnection db = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Employee";
+                return db.Query<Employee>(query);
+            }
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            using (IDbConnection db = new MySqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Employee (Name, Gender, Department, City) VALUES (@Name, @Gender, @Department, @City)";
+                db.Execute(query, new { employee.Name, employee.Gender, employee.Department, employee.City });
+            }
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            using (IDbConnection db = new MySqlConnection(connectionString))
+            {
+                string query = "UPDATE Employee SET Name = @Name, Gender = @Gender, Department = @Department, City = @City WHERE ID = @ID";
+                db.Execute(query, new { employee.Name, employee.Gender, employee.Department, employee.City, employee.ID });
+            }
+        }
+
+        public Employee GetEmployeeData(int? id)
+        {
+            using (IDbConnection db = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Employee WHERE ID = @ID";
+                return db.QuerySingleOrDefault<Employee>(query, new { ID = id });
+            }
+        }
+
+        public void DeleteEmployee(int? id)
+        {
+            using (IDbConnection db = new MySqlConnection(connectionString))
+            {
+                string query = "DELETE FROM Employee WHERE ID = @ID";
+                db.Execute(query, new { ID = id });
+            }
+        }
+    }
+}
