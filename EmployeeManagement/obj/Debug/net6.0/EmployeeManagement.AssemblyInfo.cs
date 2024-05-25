3debe91de6c81a311e319dbfe59c8427ae029b30
gphoenix14 <104388188+gphoenix14@users.noreply.github.com>
2024-05-25 15:45:13 +0200

    ok

--- EmployeeManagement/obj/Debug/net6.0/EmployeeManagement.AssemblyInfo.cs
+++ EmployeeManagement/obj/Debug/net6.0/EmployeeManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fc0a8cd783f684e3cc92429bdca768dc8ac9f04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74a3847971d8d4843045320a9c81f730f05589ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
